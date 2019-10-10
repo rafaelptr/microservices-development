187179e74e0eb0fddba11f196bbe3306e30a92b3
Rafael Petronilio <rafaelpetronilio@gmail.com>
2019-10-09 21:23:30 -0300

    crud

--- lab-2/clientes_venv/clientes.py
+++ lab-2/clientes_venv/clientes.py
@@ -32,3 +32,55 @@ def read_all():
     clientes.headers['Access-Control-Expose-Headers'] = 'Content-Range'
     clientes.headers['Content-Range'] = content_range
     return clientes
+
+def read_one(lname):
+    if lname in PEOPLE:
+        person = PEOPLE.get(lname)
+    else:
+        abort(
+            404, "Pessoa com sobrenome {lname} nao encontrada".format(lname=lname)
+        )
+    return person
+
+
+def create(person):
+    lname = person.get("lname", None)
+    fname = person.get("fname", None)
+
+    if lname not in PEOPLE and lname is not None:
+        PEOPLE[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return make_response(
+            "{lname} criado com sucesso".format(lname=lname), 201
+        )
+    else:
+        abort(
+            406,
+            "Pessoa com sobrenome {lname} ja existe".format(lname=lname),
+        )
+
+
+def update(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname")
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+
+        return PEOPLE[lname]
+    else:
+        abort(
+            404, "Pessoa com sobrenome {lname} nao encontrada".format(lname=lname)
+        )
+
+def delete(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(
+            "{lname} deletado com sucesso".format(lname=lname), 200
+        )
+    else:
+        abort(
+            404, "Pessoa com sobrenome {lname} nao encontrada".format(lname=lname)
+        )
